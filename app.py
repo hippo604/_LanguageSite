eece1a20136f4d08865c5e06ddba65de5ff78997
hippo604 <{ID}+{username}@users.noreply.github.com15911669+hippo604@users.noreply.github.com>
2021-09-01 22:19:36 -0700

    Changes to admin page and answers page, and cleanup of unused files/code

--- app.py
+++ app.py
@@ -11,102 +11,97 @@ class QuestionEditForm(FlaskForm):
     FormQuestion = StringField("Question")
     FormSubmit =SubmitField('Submit')
 
-###
-
+#Single line for the quiz (questions only)
 class QuestionForm(Form):
     question_id = StringField('ID')
     question = StringField('質問')
     answer = StringField('答え')
     comment = StringField('備考')
 
+#Single line for the quiz (includes answers only)
+class AnswerForm(Form):
+    question_id = StringField('ID')
+    question = StringField('質問')
+    answer = StringField('答え')
+    result= StringField('結果')
+
+#Multi-line form for the quiz (no answers)
 class QuizForm(FlaskForm):
-    #quiz_name = StringField('Quiz Name')
     questions = FieldList(FormField(QuestionForm), min_entries=4, max_entries=8)
 
+#Multi-line form for the quiz (includes answers)
+class QuizAnswerForm(FlaskForm):
+    questions = FieldList(FormField(AnswerForm), min_entries=4, max_entries=8)
+
+#Multi-line form for the admin page
 class AdminReviewForm(FlaskForm):
     #quiz_name = StringField('Quiz Name')
     questions = FieldList(FormField(QuestionForm), min_entries=4, max_entries=8)
 
-
-################################################################
 ################################################################
 name='Thomas'
 
-################################################################
+
 ################################################################
 @app.route('/')
 def index():
     return(render_template("basic.html",name=name))
 
-@app.route('/questions')
+
+@app.route('/questions',methods=['GET','POST'])
 def questions():
-    test = Question.query.order_by(Question.id).all()
 
-    return(render_template("questions.html",name=name,test=test))
-
-@app.route('/questions2',methods=['GET','POST'])
-def questions2():
-    question = namedtuple('question', ['question_id', 'question', 'answer'])
-
-    data = {
-        'questions' : [
-            question(3, 'Q 1', '100'),
-            question(4, 'Q 2', '50'),
-            question(5, 'Q 3', '20'),
-            question(6, 'Q 4', '80'),
-            question(7, 'Q 5', '10'),
-            question(8, 'Q 6', '80'),
-            question(9, 'Q 7', '150'),
-            question(10, 'Q 8', '10'),
-        ]
-    }
-
-    form = QuizForm(data=data)
-    if form.validate_on_submit():
-        for field in form.questions:
-            print(field.question_id.data)
-            print(field.question.data)
-            print(field.answer.data)
-
-        for value in form.questions.data:
-            print(value)
-
-    return render_template('questions2.html', form=form, name=name)
-
-@app.route('/questions3',methods=['GET','POST'])
-def questions3():
-
-    question = namedtuple('question', ['question_id', 'question', 'answer'])
+    question = namedtuple('question', ['question_id', 'question'])
 
     question_set = []
 
     Quiz1 = Quiz.query.filter_by(id=1).first()
     for q1q2 in Quiz1.questions:
-        question_set.append(question(q1q2.question.id, q1q2.question.question, q1q2.question.answer))
+        question_set.append(question(q1q2.question.id, q1q2.question.question))
 
-    data = {
-        'questions' : question_set
-    }
-
-    form = QuizForm(data=data)
-    if form.validate_on_submit():
-        for field in form.questions:
-
-            q_id = int(field.question_id.data)
-            q_q = field.question.data
-            q_a = field.answer.data
-
-            Question.query.filter_by(id=q_id).first().question = q_q
-            Question.query.filter_by(id=q_id).first().answer = q_a
-            db.session.commit()
-
-    return render_template('questions2.html', form=form, name=name)
+    return render_template('questions3.html', question_set=question_set, name=name)
 
 
-@app.route('/answers')
+@app.route('/answers',methods=['GET','POST'])
 def answers():
-    test = Question.query.order_by(Question.id).all()
-    return(render_template("answers.html",name=name,test=test))
+        question = namedtuple('question', ['question_id', 'question', 'answer', 'result'])
+
+        #Start blank list
+        question_set = []
+
+        Quiz1 = Quiz.query.filter_by(id=1).first()
+
+        for q1q2 in Quiz1.questions:
+            question_set.append(question(q1q2.question.id, q1q2.question.question, q1q2.question.answer, q1q2.result))
+
+        data = {
+            'questions' : question_set
+        }
+
+        form = QuizAnswerForm(data=data)
+        if form.validate_on_submit():
+            for field in form.questions:
+
+                try:
+                    q_id = int(field.question_id.data)
+                    print("app.py answers attempt to determine q_id")
+                except ValueError:
+                    pass
+
+                q_r = field.result.data
+
+                Quiz1 = Quiz.query.filter_by(id=1).first()
+
+                # Convoluted iteration through Association objects.
+                for q1q2 in Quiz1.questions:
+                    if q1q2.question.id == q_id:
+                        q1q2.result = q_r
+
+                db.session.commit()
+
+                q_id = None
+
+        return render_template('answers2.html', form=form, name=name)
 
 @app.route('/initialize')
 def initialize():
@@ -124,17 +119,11 @@ def initialize():
             db.session.add(q)
             db.session.commit()
 
-    Quiz1 = Quiz.query.filter_by(id=1).first()
-    question_set = []
-
-    for q1q in Quiz1.questions:
-        question_set.append(q1q.question)
-
-    return(render_template("basic.html",name=question_set))
-
+    return(render_template("basic.html",name=name))
 
 @app.route('/admin',methods=['GET','POST'])
 def admin():
+
     question = namedtuple('question', ['question_id', 'question', 'answer', 'comment'])
 
     question_set = []
@@ -148,20 +137,37 @@ def admin():
     }
 
     form = QuizForm(data=data)
+
     if form.validate_on_submit():
         for field in form.questions:
 
-            q_id = int(field.question_id.data)
+            #Need to make sure blank rows don't make it into the db
+            try:
+                q_id = int(field.question_id.data)
+            except ValueError:
+                pass
+
+            # Capturing items in Quick field
             q_q = field.question.data
             q_a = field.answer.data
             q_c = field.comment.data
 
-            Question.query.filter_by(id=q_id).first().question = q_q
-            Question.query.filter_by(id=q_id).first().answer = q_a
-            Question.query.filter_by(id=q_id).first().comment = q_c
-            db.session.commit()
+            try:
+                question_assembled = Question.query.filter_by(id=q_id).first()
+            except ValueError:
+                pass
 
-    return(render_template("admin.html", form=form))
+            if question_assembled != None and q_id != "" and q_q != "" and q_a != "":
+                question_assembled.question = q_q
+                question_assembled.answer = q_a
+                question_assembled.comment = q_c
+
+            #Don't want question_assembled bleeding into the next iteration
+            question_assembled = None
+
+        db.session.commit()
+
+    return(render_template("admin.html", form=form, name=name))
 
 if __name__ == '__main__':
     app.run(debug=True)
